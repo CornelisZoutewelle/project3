506fe8705edae6f361b175a89265bd1a3616b21a
Cornelis Zoutewelle <9018186@student.zadkine.nl>
2023-01-30 13:56:33 +0100

    HTML to PHP

--- Niels/subpagina1.php
+++ Niels/subpagina1.php
@@ -43,7 +43,7 @@
         </nav>
         <section>
             <?php
-                echo "The time is " . date("h:i:sa");
+                echo "Today is " . date("Y/m/d") . "</br>";
             ?>
         </section>
     </main>
