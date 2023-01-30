9afe7cbd9133b2c1ddff1f0c8b93d4e5ec7cf38f
MaohuaFan <9019910@student.zadkine.nl>
2023-01-30 14:56:07 +0100

    php nav

--- index.php
+++ index.php
@@ -9,9 +9,38 @@
 </head>
 <body>
     <main>
-        <?php
-            include 'nav.php';
-        ?>
+        <nav>
+            <ul>
+                <li><a href="../project3/index.php">Home</a></li>
+                <div class="space_between">
+                    <li><div class="subnav">
+                            <button class="subnavbtn">Niels</button>
+                            <div class="subnav-content">
+                                <a href="../project3/Niels/Subpagina1.php">Subpagina1</a>
+                            </div>
+                        </div></li>
+                    <li><div class="subnav">
+                            <button class="subnavbtn">Lorenzo</button>
+                            <div class="subnav-content">
+                                <a href="../project3/Lorenzo/Subpagina2.php">Subpagina2</a>
+                            </div>
+                        </div></li> 
+                    <li><div class="subnav">
+                            <button class="subnavbtn">Maohua</button>
+                            <div class="subnav-content">
+                                <a href="../project3/Maohua/Subpagina3.php">Subpagina3</a>
+                            </div>
+                        </div></li>
+                    <li><div class="subnav">
+                            <button class="subnavbtn">Milan</button>
+                                <div class="subnav-content">
+                                    <a href="../project3/Milan/Subpagina4.php">Subpagina4</a>
+                                </div>
+                        </div>
+                    </li>
+                </div>
+            </ul>
+        </nav>
         <section>
             <h1>Homepagina</h1>
         </section>
--- nav.php
+++ nav.php
@@ -1,29 +1,29 @@
         <nav>
             <ul>
-                <li><a href="../project3/index.php">Home</a></li>
+                <li><a href="../index.php">Home</a></li>
                 <div class="space_between">
                     <li><div class="subnav">
                             <button class="subnavbtn">Niels</button>
                             <div class="subnav-content">
-                                <a href="../project3/Niels/Subpagina1.php">Subpagina1</a>
+                                <a href="../Niels/Subpagina1.php">Subpagina1</a>
                             </div>
                         </div></li>
                     <li><div class="subnav">
                             <button class="subnavbtn">Lorenzo</button>
                             <div class="subnav-content">
-                                <a href="../project3/Lorenzo/Subpagina2.php">Subpagina2</a>
+                                <a href="../Lorenzo/Subpagina2.php">Subpagina2</a>
                             </div>
                         </div></li> 
                     <li><div class="subnav">
                             <button class="subnavbtn">Maohua</button>
                             <div class="subnav-content">
-                                <a href="../project3/Maohua/Subpagina3.php">Subpagina3</a>
+                                <a href="../Maohua/Subpagina3.php">Subpagina3</a>
                             </div>
                         </div></li>
                     <li><div class="subnav">
                             <button class="subnavbtn">Milan</button>
                                 <div class="subnav-content">
-                                    <a href="../project3/Milan/Subpagina4.php">Subpagina4</a>
+                                    <a href="../Milan/Subpagina4.php">Subpagina4</a>
                                 </div>
                         </div>
                     </li>
