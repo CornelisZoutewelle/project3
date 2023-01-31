b2dbe7e2c66e133037bd69b89c898e3a8efb5811
MaohuaFan <9019910@student.zadkine.nl>
2023-01-31 11:11:09 +0100

    php

--- Maohua/action.php
+++ Maohua/action.php
@@ -13,8 +13,10 @@
             <?php
                 $name = $_POST['name']; /*Store username in variable*/
                 $pass = $_POST['pass']; /*Store password in variable*/
-                if(strlen($name) <= 5) {    /*Print foutmelding als username <5 tekens*/
-                    echo"Error the username is less than 5 characters";
+                if(strlen($name) <= 5) {    /*Print foutmelding als username kleiner dan of gelijk aan 5 tekens*/
+                    echo"Error the username is less than 5 characters <br>";
+                    echo "Username: $name<br>"; /*Print Username*/
+                    echo "Password: $pass<br>"; /*Print Password*/
                 } else {
                     echo "Username: $name<br>"; /*Print Username*/
                     echo "Password: $pass<br>"; /*Print Password*/
