beaac7f3fe5aa77a6e2205578b2013bf8d5f89bf
MaohuaFan <9019910@student.zadkine.nl>
2023-02-20 12:58:48 +0100

    php

--- Maohua/functions.php
+++ Maohua/functions.php
@@ -25,6 +25,27 @@ function ConnectDb(){
 function OvzBieren(){
     echo"<br><br><br>";
     echo"overzicht<br>";
-
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=bieren", "root", "");
+        $query = $db->prepare("SELECT * FROM bier");
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        echo"<table>";
+            echo "<tr>";
+                echo "<td>" . "Bier Code " . "</td>";
+                echo "<td>" . "Bier Naam " . "</td>";
+                echo "<td>" . "Alcohol % " . "</td>";
+            echo "</tr>";
+            foreach($result as &$data) {
+                echo "<tr>";
+                    echo "<td>" . $data["biercode"] . "</td>";
+                    echo "<td>" . $data["naam"] . "</td>";
+                    echo "<td>" . $data["alcohol"] . "</td>";
+                echo "</tr>";
+            }
+        echo"</table>";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
 }
 ?>
