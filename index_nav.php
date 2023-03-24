4bc8bf833347f71a79ff0201192decfcc44dc2b6
Lorenzo-Van-Der-Horst <9019456@student.zadkine.nl>
2023-03-24 14:00:47 +0100

    Update index_nav.php

--- index_nav.php
+++ index_nav.php
@@ -3,7 +3,27 @@
         <li><a href="index.php"><img src="img/Logo.png" alt="Logo" style="width:80px ;height:45px;"></a></li>
             <div class="space_between">
         <li><div class="subnav">
-                        
+
+        <<<<<<< HEAD
+
+=======
+                <li><div class="subnav">
+                    <button class="subnavbtn">Model S</button>
+                <div class="subnav-content">
+                    <a href="">Model S</a>
+                </div>
+            </div></li>
+            <li><div class="subnav">
+                        <button class="subnavbtn">Klachten</button>
+                        <div class="subnav-content">
+                            <a href="klachten_product.php">Klacht Product</a>
+                            <a href="klachten_website.php">Klacht Website</a>
+                            <a href="klachten_medewerker.php">Klacht Medewerker</a>
+                    </div>
+                </div>
+            </li>
+>>>>>>> 08f95b149dbd5abebf6c9836aca963f34f2406e0
+
                         </div>
                     </li>
                 </div>
