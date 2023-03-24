9feee1999a2c10a1a08645bc8e6e59161f1c4e54
MaohuaFan <9019910@student.zadkine.nl>
2023-03-24 14:03:56 +0100

    php

--- nav.php
+++ nav.php
@@ -1,42 +1,40 @@
 <nav>
-            <ul>
-                <li><a href="../index.php">Home</a></li>
-                <div class="space_between">
-                <li><div class="subnav">
-                            <button class="subnavbtn">Klachten</button>
-                            <div class="subnav-content">
-                                <a href="../klachten_product.php">Klacht Product</a>
-                                <a href="../klachten_website.php">Klacht Website</a>
-                                <a href="../klachten_medewerker.php">Klacht Medewerker</a>
-                            </div>
-                        </div>
-                </li>
-                <li><div class="subnav">
-                        <button class="subnavbtn">Niels</button>
-                        <div class="subnav-content">
-                            <a href="../Niels/Subpagina1.php">Subpagina1</a>
-                        </div>
-                    </div></li>
-                <li><div class="subnav">
-                        <button class="subnavbtn">Lorenzo</button>
-                        <div class="subnav-content">
-                            <a href="../Lorenzo/Subpagina2.php">Subpagina2</a>
-                        </div>
-                    </div></li> 
-                <li><div class="subnav">
-                        <button class="subnavbtn">Maohua</button>
-                        <div class="subnav-content">
-                            <a href="../Maohua/Subpagina3.php">Subpagina3</a>
-                        </div>
-                    </div></li>
-                <li><div class="subnav">
-                        <button class="subnavbtn">Milan</button>
-                            <div class="subnav-content">
-                                <a href="../Milan/Subpagina4.php">Subpagina4</a>
-                            </div>
-                    </div>
-                </li>
-            </div>
-        </ul>
-    </nav>
+    <ul>
+        <li><a href="../index.php">Home</a></li>
+        <div class="space_between">
+            <li><div class="subnav">
+                <button class="subnavbtn">Klachten</button>
+                <div class="subnav-content">
+                    <a href="../klachten_product.php">Klacht Product</a>
+                    <a href="../klachten_website.php">Klacht Website</a>
+                    <a href="../klachten_medewerker.php">Klacht Medewerker</a>
+                </div>
+            </div></li>
+            <li><div class="subnav">
+                <button class="subnavbtn">Niels</button>
+                <div class="subnav-content">
+                    <a href="../Niels/Subpagina1.php">Subpagina1</a>
+                </div>
+            </div></li>
+            <li><div class="subnav">
+                <button class="subnavbtn">Lorenzo</button>
+                <div class="subnav-content">
+                    <a href="../Lorenzo/Subpagina2.php">Subpagina2</a>
+                </div>
+            </div></li> 
+            <li><div class="subnav">
+                <button class="subnavbtn">Maohua</button>
+                <div class="subnav-content">
+                    <a href="../Maohua/Subpagina3.php">Subpagina3</a>
+                </div>
+            </div></li>
+            <li><div class="subnav">
+                <button class="subnavbtn">Milan</button>
+                <div class="subnav-content">
+                    <a href="../Milan/Subpagina4.php">Subpagina4</a>
+                </div>
+            </div></li>
+        </div>
+    </ul>
+</nav>
         
