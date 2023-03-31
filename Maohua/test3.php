41fd8600e49e2e0754335c000f14408904b2020e
MaohuaFan <9019910@student.zadkine.nl>
2023-03-31 10:08:02 +0200

    php

---
+++ Maohua/test3.php
@@ -0,0 +1,4 @@
+<?php
+    echo'<a href="https://static-assets.tesla.com/configurator/compositor?context=design_studio_2&options='.$MTS13,$PPSW,$WS91,$IBE00.'&view=FRONT34&model=ms&size=1920"></a>'; //&bkba_opt=1&crop=1300,500,300,300&
+?>
+
