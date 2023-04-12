145dff0fabecb90a3fc4f033c04d955e002de17b
Lorenzo-Van-Der-Horst <9019456@student.zadkine.nl>
2023-04-12 13:15:01 +0200

    Update Product_page.php

--- Product_page.php
+++ Product_page.php
@@ -16,7 +16,8 @@
         ?>
 </header>
 
-<h1>Tesla Model S</h1>
+<div class="a">
+<h1>Tesla Model S</h1><br>
     <form action="Betalen.php" method="get">
     <input type="checkbox"  name="keuze[]" value="1"/>Set winterbanden: € 4.000<br/>
     <input type="checkbox"  name="keuze[]" value="2"/>Enhanced Autopilot: € 3.800<br/>
@@ -25,12 +26,22 @@
 
         <input type="submit" value="Bestellen"/>
     </form>
-
+    </div>
     
-
+    <img class="Product" src="https://static-assets.tesla.com/configurator/compositor?context=design_studio_2&options=$MTS13,$PPSW,$WS90,$IBE00&view=FRONT34&model=ms&size=1920&bkba_opt=1&crop=1300,550,300,250&" alt="Product" style="width5000px;height:500px;margin-left:15px;">
 <?php
-     /*   include 'footer.php'; */
+       include 'footer.php'; 
 ?>
+<style>
+img {
+  float: right;
+}
+
+
+div.a {
+  font-size: 15px;
+}
+</style>
 
 </body>
 </html>
